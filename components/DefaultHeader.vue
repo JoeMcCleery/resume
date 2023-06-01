d43d108620abe8472fb4ddd4091e1312e6c6bc19
JoeMcCleery <mccleery.joseph@gmail.com>
2023-06-01 16:50:54 +0800

    Changed header implementation

--- components/DefaultHeader.vue
+++ components/DefaultHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="fixed w-full h-24 shadow-lg backdrop-blur-lg z-50">
     <div class="absolute h-full w-full opacity-50 -z-10 bg-background" />
-    <div class="container p-4 h-full mx-auto flex items-center justify-between">
+    <div class="relative container p-4 h-full mx-auto flex items-center justify-start space-x-8">
       <NuxtLink to="/">
         <h1 class="text-3xl font-bold">
           {{ $config.public.websiteTitle }}
@@ -9,7 +9,7 @@
       </NuxtLink>
       <nav>
         <ul class="flex space-x-8 text-lg font-bold">
-          <li v-for="story in data.stories" :key="story.id">
+          <li v-for="story in stories" :key="story.id">
             <NuxtLink :to="`/${story.slug}`">
               {{ story.content.navigation_button_text || story.name }}
             </NuxtLink>
@@ -23,14 +23,15 @@
 <script setup lang="ts">
 const config = useRuntimeConfig()
 
-const { data } = await useStoryblokApi().getStories({
-  starts_with: 'pages/',
-  filter_query: {
-    show_navigation_link: {
-      in: true
-    }
-  },
+const { data } = await useStoryblokApi().getStory('navigation-config', {
+  resolve_relations: [
+    'pages'
+  ],
   version: config.public.storyblokVersion
+})
+
+const stories = computed(() => {
+  return data.story.content.pages.map((uuid: string) => data.rels.find(story => story.uuid === uuid))
 })
 </script>
 
