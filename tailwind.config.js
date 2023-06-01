1a970fc00d1c9330dd990fdf3f24acaacae8e5cf
JoeMcCleery <mccleery.joseph@gmail.com>
2023-06-01 15:23:39 +0800

    Add custom prose styling using colour context

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,46 @@ module.exports = {
     fontFamily:
     {
       roboto: 'Roboto'
+    },
+    extend: {
+      typography: theme => ({
+        DEFAULT: {
+          css: {
+            '--tw-prose-body': `brightness(${theme('colors.text')}, 0.7)`,
+            '--tw-prose-headings': `brightness(${theme('colors.text')}, 0.9)`,
+            '--tw-prose-lead': `brightness(${theme('colors.text')}, 0.6)`,
+            '--tw-prose-links': `brightness(${theme('colors.text')}, 0.9)`,
+            '--tw-prose-bold': `brightness(${theme('colors.text')}, 0.9)`,
+            '--tw-prose-counters': `brightness(${theme('colors.text')}, 0.5)`,
+            '--tw-prose-bullets': `brightness(${theme('colors.text')}, 0.3)`,
+            '--tw-prose-hr': `brightness(${theme('colors.text')}, 0.2)`,
+            '--tw-prose-quotes': `brightness(${theme('colors.text')}, 0.9)`,
+            '--tw-prose-quote-borders': `brightness(${theme('colors.text')}, 0.2)`,
+            '--tw-prose-captions': `brightness(${theme('colors.text')}, 0.5)`,
+            '--tw-prose-code': `brightness(${theme('colors.text')}, 0.9)`,
+            '--tw-prose-pre-code': `brightness(${theme('colors.text')}, 0.2)`,
+            '--tw-prose-pre-bg': `brightness(${theme('colors.background')}, 0.8)`,
+            '--tw-prose-th-borders': `brightness(${theme('colors.text')}, 0.3)`,
+            '--tw-prose-td-borders': `brightness(${theme('colors.text')}, 0.2)`,
+            '--tw-prose-invert-body': `brightness(${theme('colors.text')}, 0.3)`,
+            '--tw-prose-invert-headings': `brightness(${theme('colors.text')}, 1)`,
+            '--tw-prose-invert-lead': `brightness(${theme('colors.text')}, 0.4)`,
+            '--tw-prose-invert-links': `brightness(${theme('colors.text')}, 1)`,
+            '--tw-prose-invert-bold': `brightness(${theme('colors.text')}, 1)`,
+            '--tw-prose-invert-counters': `brightness(${theme('colors.text')}, 0.4)`,
+            '--tw-prose-invert-bullets': `brightness(${theme('colors.text')}, 0.6)`,
+            '--tw-prose-invert-hr': `brightness(${theme('colors.text')}, 0.7)`,
+            '--tw-prose-invert-quotes': `brightness(${theme('colors.text')}, 0.1)`,
+            '--tw-prose-invert-quote-borders': `brightness(${theme('colors.text')}, 0.7)`,
+            '--tw-prose-invert-captions': `brightness(${theme('colors.text')}, 0.4)`,
+            '--tw-prose-invert-code': `brightness(${theme('colors.text')}, 1)`,
+            '--tw-prose-invert-pre-code': `brightness(${theme('colors.text')}, 0.3)`,
+            '--tw-prose-invert-pre-bg': `opacity(brightness(${theme('colors.background')}, 0.8), 0.5)`,
+            '--tw-prose-invert-th-borders': `brightness(${theme('colors.text')}, 0.6)`,
+            '--tw-prose-invert-td-borders': `brightness(${theme('colors.text')}, 0.7)`
+          }
+        }
+      })
     }
   }
 }
