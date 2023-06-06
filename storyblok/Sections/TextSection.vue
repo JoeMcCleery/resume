f755a9d4c482406c2cde4b4b9983480ab2521749
JoeMcCleery <mccleery.joseph@gmail.com>
2023-06-06 16:05:16 +0800

    Added TextSection

---
+++ storyblok/Sections/TextSection.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    v-if="blok"
+    v-editable="blok"
+    class="relative container m-auto px-4 py-16 text-text bg-background"
+    :style="blok.override_colours ? cssVars : ''"
+  >
+    <RichText :text="blok.text" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  blok: {
+    type: Object,
+    required: true
+  }
+})
+
+const sectionColours = ref({ textColour: '255 255 255', backgroundColour: '128 128 128' })
+
+sectionColours.value = {
+  textColour: hexToRgb(props.blok.text_colour.color),
+  backgroundColour: hexToRgb(props.blok.background_colour.color)
+}
+
+const cssVars = computedCssVars(sectionColours)
+</script>
