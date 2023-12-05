bd6e2ce426b9449196eb0a8e8e9e723cf7068f6f
JoeMcCleery <mccleery.joseph@gmail.com>
2023-12-05 14:56:13 +0800

    Add image component

---
+++ components/ImageComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <nuxt-img
+    v-if="image.id"
+    provider="storyblok"
+    format="webp"
+    :src="image.filename"
+    :title="image.title"
+    :alt="image.alt"
+    :copyright="image.copyright"
+    :source="image.source"
+  />
+</template>
+
+<script setup lang="ts">
+defineProps({
+  image: {
+    type: Object,
+    required: true
+  }
+})
+</script>
