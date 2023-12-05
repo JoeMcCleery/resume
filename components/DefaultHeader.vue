8f581125f699465c5504724a328c6dc9e410f12d
JoeMcCleery <mccleery.joseph@gmail.com>
2023-12-05 20:02:35 +0800

    Simplify default header layout

--- components/DefaultHeader.vue
+++ components/DefaultHeader.vue
@@ -1,13 +1,9 @@
 <template>
   <header class="fixed w-full h-24 backdrop-blur-lg z-50 text-context border-b-2 border-context">
     <div class="absolute h-full w-full opacity-50 -z-10 bg-context" />
-    <div class="relative container p-4 h-full w-full mx-auto flex items-center justify-between space-x-8">
-      <div class="h-full flex items-center justify-center space-x-8">
-        <SiteNavigation />
-      </div>
-      <div class="h-full flex items-center justify-center space-x-8">
-        <DarkModeToggle />
-      </div>
+    <div class="relative container p-4 h-full w-full mx-auto flex items-center justify-between">
+      <SiteNavigation />
+      <DarkModeToggle />
     </div>
   </header>
 </template>
