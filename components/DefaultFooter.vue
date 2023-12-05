0ea94fa9b075c7716b1417f3134c1f867a21262e
JoeMcCleery <mccleery.joseph@gmail.com>
2023-12-05 14:57:26 +0800

    Remove dark mode toggle from footer

--- components/DefaultFooter.vue
+++ components/DefaultFooter.vue
@@ -1,11 +1,8 @@
 <template>
   <footer class="relative w-full bg-context text-context border-t-2 border-context">
-    <div class="w-full h-24 p-4 container mx-auto flex items-center justify-between space-x-8">
+    <div class="w-full h-24 p-4 container mx-auto">
       <div class="h-full flex items-center justify-center space-x-8">
         <ContactLinks />
-      </div>
-      <div class="h-full flex items-center justify-center space-x-8">
-        <DarkModeToggle />
       </div>
     </div>
   </footer>
