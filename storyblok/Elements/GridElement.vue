d255b9a13ecd013f314d8468ac2f9bd3ba753d0e
JoeMcCleery <mccleery.joseph@gmail.com>
2023-12-05 15:27:55 +0800

    Lint grid element

--- storyblok/Elements/GridElement.vue
+++ storyblok/Elements/GridElement.vue
@@ -5,9 +5,9 @@
   >
     <div v-if="blok.content.length > 0" class="w-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
       <StoryblokComponent
-          v-for="element in blok.content"
-          :key="element._uid"
-          :blok="element"
+        v-for="element in blok.content"
+        :key="element._uid"
+        :blok="element"
       />
     </div>
   </div>
