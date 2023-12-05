16ba7bce9038352ec91525494a8624dd3fed9aa1
JoeMcCleery <mccleery.joseph@gmail.com>
2023-12-05 21:06:34 +0800

    Begin change to new site navigation with mobile support

--- components/DefaultHeader.vue
+++ components/DefaultHeader.vue
@@ -1,9 +1,15 @@
 <template>
-  <header class="fixed w-full h-24 backdrop-blur-lg z-50 text-context border-b-2 border-context">
+  <header class="fixed w-full h-content min-h-24 backdrop-blur-lg z-50 text-context border-b-2 border-context">
     <div class="absolute h-full w-full opacity-50 -z-10 bg-context" />
-    <div class="relative container p-4 h-full w-full mx-auto flex items-center justify-between">
-      <SiteNavigation />
+    <div class="container p-4 h-full w-full mx-auto flex items-center justify-between">
+      <SiteNavigation class="hidden sm:flex space-x-4" />
+      <SiteNavigation class="flex flex-col sm:hidden space-y-4" />
       <DarkModeToggle />
     </div>
   </header>
 </template>
+
+<script setup lang="ts">
+import { Bars3Icon } from '@heroicons/vue/24/solid'
+
+</script>
