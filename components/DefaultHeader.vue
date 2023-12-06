5c02bcd88e79d4207fd7e679b27b3fa32078ff47
JoeMcCleery <mccleery.joseph@gmail.com>
2023-12-06 15:01:41 +0800

    Improved mobile header

--- components/DefaultHeader.vue
+++ components/DefaultHeader.vue
@@ -1,14 +1,36 @@
 <template>
-  <header class="fixed w-full h-content min-h-24 backdrop-blur-lg z-50 text-context border-b-2 border-context">
+  <header class="fixed w-full backdrop-blur-lg z-50 text-context border-b-2 border-context">
     <div class="absolute h-full w-full opacity-50 -z-10 bg-context" />
-    <div class="container p-4 h-full w-full mx-auto flex items-center justify-between">
-      <SiteNavigation class="hidden sm:flex space-x-4" />
-      <SiteNavigation class="flex flex-col sm:hidden space-y-4" />
-      <DarkModeToggle />
+    <div class="container w-full mx-auto flex flex-col">
+      <div class="p-4 flex w-full items-center justify-between h-24">
+        <Button
+          class="sm:hidden flex justify-center items-center text-2xl px-4 py-2 text-context rounded-full border-2 border-context default-focus"
+          @click="showNav = !showNav"
+        >
+          <span>Menu</span>
+          <Bars3Icon
+            class="ml-2 h-8 w-8"
+            aria-hidden="true"
+          />
+        </Button>
+        <SiteNavigation class="hidden sm:flex space-x-4" />
+        <DarkModeToggle />
+      </div>
+      <div
+        class="sm:hidden justify-center items-center w-full p-4 border-t-2 border-context"
+        :class="showNav ? 'flex' : 'hidden'"
+      >
+        <SiteNavigation
+          class="flex-col space-y-4"
+          @selection-made="showNav = false"
+        />
+      </div>
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
-/* import { Bars3Icon } from '@heroicons/vue/24/solid' */
+import { Bars3Icon } from '@heroicons/vue/24/solid'
+
+const showNav = ref(false)
 </script>
