67d0e55eeb0099bfee13af997718ae5ba6985781
JoeMcCleery <mccleery.joseph@gmail.com>
2023-12-07 16:50:07 +0800

    Updates

---
+++ _nuxt/useAsyncStoryblok.6829021c.js
@@ -0,0 +1,1 @@
+import{aa as c,ac as r,A as i,ad as y}from"./entry.5b8e1177.js";const d=async(s,e={},o={})=>{const n=`${JSON.stringify(e)}${s}`,a=c(`${n}-state`),u=r();if(i(()=>{a.value&&a.value.id&&y(a.value.id,t=>a.value=t,o)}),!a.value){const{data:t}=await u.get(`cdn/stories/${s}`,e);a.value=t.story}return a};export{d as u};
