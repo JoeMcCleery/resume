c52ea56b9a9d7e259dbf82e8287c84ce12a59eb3
JoeMcCleery <mccleery.joseph@gmail.com>
2023-12-07 16:49:19 +0800

    Added simple divider section

---
+++ storyblok/Sections/DividerSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <SectionContainer
+    v-if="blok"
+    v-editable="blok"
+    :blok="blok"
+    class="flex justify-center items-center border-t border-b border-context"
+    :style="`height: ${blok.height/4}rem`"
+  >
+    <hr class="w-full border-context">
+  </SectionContainer>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  blok: {
+    type: Object,
+    required: true
+  }
+})
+</script>
