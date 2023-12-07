87b217bfa689f513a586e194baee866255b717a3
JoeMcCleery <mccleery.joseph@gmail.com>
2023-12-07 16:48:35 +0800

    adjust grid element sizing

--- storyblok/Elements/GridElement.vue
+++ storyblok/Elements/GridElement.vue
@@ -3,7 +3,7 @@
     v-if="blok"
     v-editable="blok"
   >
-    <div v-if="blok.content.length > 0" class="w-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
+    <div v-if="blok.content.length > 0" class="w-full grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
       <StoryblokComponent
         v-for="element in blok.content"
         :key="element._uid"
